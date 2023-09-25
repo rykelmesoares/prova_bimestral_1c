086730359c18f59d386bd166bc0c427d1a395d9e
rykelmesoares <143404132+rykelmesoares@users.noreply.github.com>
2023-09-25 08:48:35 -0300

    prova_de_sergio
    Aqui é a nossa prova da dupla: Aila e Rykelme

---
+++ sergio_lavor/prova001.py
@@ -0,0 +1,8 @@
+n1=str( input('digite seu nome:'))
+#aqui é o nome da pessoa
+n2=int( input('digite sua idade:'))
+#aqui é a idade da pessoa
+n3=float( input('digite seu peso:'))
+#aqui é o peso da pessoa
+n4=float( input('digite sua altura:'))
+#aqui é a altura da pessoa
