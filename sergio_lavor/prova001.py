c93b5827e561a840b6d6e0bb3092e8f1bc944264
rykelmesoares <143404132+rykelmesoares@users.noreply.github.com>
2023-09-25 09:11:21 -0300

    sergio_lavor
    continuidade da prova: Aila e Rykelme

--- sergio_lavor/prova001.py
+++ sergio_lavor/prova001.py
@@ -1,8 +1,12 @@
-n1=str( input('digite seu nome:'))
+nome=str( input('digite seu nome:'))
 #aqui é o nome da pessoa
-n2=int( input('digite sua idade:'))
+idade=int( input('digite sua idade:'))
 #aqui é a idade da pessoa
-n3=float( input('digite seu peso:'))
+peso=float( input('digite seu peso:'))
 #aqui é o peso da pessoa
-n4=float( input('digite sua altura:'))
+altura=float( input('digite sua altura:'))
 #aqui é a altura da pessoa
+print("olá", nome)
+print("sua", idade)
+print("seu", peso)
+print("sua", altura)
